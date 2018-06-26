46fb1a9bbd3617b06523035efb6075b2673d1437
maijuntian <jtmai@xtremeprog.com>
2018-06-26 22:52:05 +0800

    new

---
+++ app/src/main/java/com/mai/dibbler/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mai.dibbler.activity;
+
+import android.os.Bundle;
+
+import com.mai.dibbler.base.BaseActivity;
+import com.mai.dibbler.base.BaseDelegate;
+
+public class MainActivity extends BaseActivity<BaseDelegate> {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
